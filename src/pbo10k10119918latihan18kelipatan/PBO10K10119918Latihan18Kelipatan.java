fd4b8e4bc4e65220a7a5abc5cfb80d5bb5960ac9
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-10-30 12:40:19 +0700

    PBO10K-10119918-Latihan18-Kelipatan

---
+++ src/pbo10k10119918latihan18kelipatan/PBO10K10119918Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k10119918latihan18kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Andreas Suryadi
+ * KELAS    : IF-10K
+ * NIM      : 10119918
+ * Deskripsi Program : Menghitung Kelipatan
+ *
+ */
+
+public class PBO10K10119918Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i = 3.5;
+
+        while (i <= 35) {
+            System.out.println(i);
+
+            i += 3.5;
+        }
+        
+    }
+    
+}
